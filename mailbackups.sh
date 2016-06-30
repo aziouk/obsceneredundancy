fa72818029aacc786bca3baf72380ff0c6e91e5f
Blake Moore <blake@blakemoore.co.uk>
2016-06-30 10:37:54 +0100

    Using the same mail format as in the MultiDCBackup

--- mailbackups.sh
+++ mailbackups.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-./listbackups.sh >  listbackups.txt
+# SET ROOT PATH
+cd /root/automation/multi-dc-backup
+PWD=$(pwd)
+
+# LOAD CONFIGURATION FILE
+source "$PWD/config.conf"
 
 HOST=$(hostnamectl --static)
 DATE=$(date +%Y-%M-%d)
 
-
-mail -s "Obscene Backup Report: $DATE" -r "obscenebackup@$HOST" adam.bull@rackspace.co.uk < listbackups.txt
+if [ "$EMAIL_RESULTS" != "false" ]; then
+    echo "Sending emails"
+    HOST=$(hostnamectl --static)
+    ./listbackups.sh | mail -s "Obscene Backup Report: $DATE" -r "obscenebackup@$HOST" "$EMAIL_RESULTS"
+else
+    echo "No confirmation email sent"
+fi
